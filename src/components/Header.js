538c4ba086c207c58a888bfaf04e0deefe664c1f
barakajoshua <balakajoshuah@gmail.com>
2025-05-14 00:47:12 +0200

    all the work

--- src/components/Header.js
+++ src/components/Header.js
@@ -47,7 +47,7 @@ export default async function Navbar() {
           
            <button
                onClick={() => signOut({ callbackUrl: "/login" })}
-               className="px-4 py-2 bg-red-500 text-white rounded-full"
+               className="px-4 py-2 bg-blue-500 text-white rounded-full"
             >
                Logout
             </button>
