2558053838c821eb3efd2d798447afec17faef72
barakajoshua <balakajoshuah@gmail.com>
2025-05-14 08:59:37 +0200

    member sluges

--- src/components/Header.js
+++ src/components/Header.js
@@ -49,7 +49,7 @@ export default async function Navbar() {
                onClick={() => signOut({ callbackUrl: "/login" })}
                className="px-4 py-2 bg-blue-500 text-white rounded-full"
             >
-               Logout
+               Join Us
             </button>
 
           
